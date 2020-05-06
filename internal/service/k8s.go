115ca3ef1ea6b0f67f408c9035dfb776399ae30c
Casey Entzi <casey.entzi@silverblueprints.com>
2020-05-06 17:17:28 -0500

    testing a deployment

--- internal/service/k8s.go
+++ internal/service/k8s.go
@@ -7,7 +7,8 @@ import (
 	"gitlab.unanet.io/devops/eve/pkg/errors"
 	"gitlab.unanet.io/devops/eve/pkg/eve"
 	"go.uber.org/zap"
-	k8sErros "k8s.io/apimachinery/pkg/api/errors"
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -18,6 +19,8 @@ import (
 const (
 	DockerRepoFormat = "unanet-%s.jfrog.io"
 )
+
+func int32Ptr(i int32) *int32 { return &i }
 
 func getDockerImagePath(artifact eve.DeployArtifact) string {
 	repo := fmt.Sprintf(DockerRepoFormat, artifact.ArtifactoryFeed)
@@ -45,26 +48,46 @@ func (s *Scheduler) deployDockerService(ctx context.Context, secrets vault.Secre
 		return
 	}
 
-	// get pods in all the namespaces by omitting namespace
-	// Or specify namespace to get pods in particular namespace
-	pods, err := client.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "demo-deployment",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(2),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "demo",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "demo",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "web",
+							Image: "nginx:1.12",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-	// Examples for error handling:
-	// - Use helper functions e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	_, err = client.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
-	if k8sErros.IsNotFound(err) {
-		fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-	} else if statusError, isStatus := err.(*k8sErros.StatusError); isStatus {
-		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-	} else if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Printf("Found example-xxxxx pod in default namespace\n")
+	deploymentsClient := client.AppsV1().Deployments(apiv1.NamespaceDefault)
+	_, err = deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		s.Logger(ctx).Error("error running deploy", zap.Error(err))
 	}
 
 }
