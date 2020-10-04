3a55093a85b745565dc48b1b7b9659479deea664
Troy Sampson <troy.j.sampson@gmail.com>
2020-10-04 12:57:11 -0600

    revert that nonsense

--- internal/service/k8s-deploy.go
+++ internal/service/k8s-deploy.go
@@ -8,23 +8,19 @@ import (
 	"strings"
 	"time"
 
-	"go.uber.org/zap"
-
-	"gitlab.unanet.io/devops/eve-sch/internal/config"
 	"gitlab.unanet.io/devops/eve/pkg/eve"
+	"go.uber.org/zap"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/client-go/kubernetes"
+
+	"gitlab.unanet.io/devops/eve-sch/internal/config"
 )
 
 const (
-	DockerRepoFormat           = "unanet-%s.jfrog.io"
-	dockerCreds                = "docker-cfg"
-	fsGroup                    = 65534
-	terminationGracePeriodSecs = 300
+	DockerRepoFormat = "unanet-%s.jfrog.io"
 )
 
 func int32Ptr(i int) *int32 {
@@ -34,286 +30,216 @@ func int32Ptr(i int) *int32 {
 
 func int64Ptr(i int64) *int64 { return &i }
 
-type k8sServiceOption func(*apiv1.Service)
-
-func svcNameOpt(serviceName string) k8sServiceOption {
-	return func(s *apiv1.Service) {
-		svcNilCheck(s)
-		s.ObjectMeta.Name = serviceName
-		if s.Spec.Selector == nil {
-			s.Spec.Selector = map[string]string{
-				"app": serviceName,
-			}
-		} else {
-			s.Spec.Selector["app"] = serviceName
-		}
-	}
-}
-
-func svcNamespaceOpt(ns string) k8sServiceOption {
-	return func(s *apiv1.Service) {
-		svcNilCheck(s)
-		s.ObjectMeta.Namespace = ns
-	}
-}
-
-func svcPortOpt(port int) k8sServiceOption {
-	return func(s *apiv1.Service) {
-		svcPort := apiv1.ServicePort{
-			Port: int32(port),
-			TargetPort: intstr.IntOrString{
-				Type:   intstr.Int,
-				IntVal: int32(port),
-			},
-		}
-		svcNilCheck(s)
-		if s.Spec.Ports == nil {
-			s.Spec.Ports = []apiv1.ServicePort{svcPort}
-		} else {
-			s.Spec.Ports = append(s.Spec.Ports, svcPort)
-		}
-	}
-}
-
-func svcStickySessionsOpt(stickySessions bool) k8sServiceOption {
-	return func(s *apiv1.Service) {
-		if stickySessions {
-			svcNilCheck(s)
-			s.Spec.SessionAffinity = apiv1.ServiceAffinityClientIP
-		}
-	}
-}
-
-func svcNilCheck(svc *apiv1.Service) *apiv1.Service {
-	if svc == nil {
-		return &apiv1.Service{}
-	} else {
-		return svc
-	}
-
-}
-
-func newK8sService(opts ...k8sServiceOption) *apiv1.Service {
-	s := &apiv1.Service{}
-	for _, opt := range opts {
-		opt(s)
-	}
-	return s
-}
-
-type K8sDeployOption func(*appsv1.Deployment)
-
-func deploymentContainerOpt(
-	appPort, metricsPort int,
-	name, image string,
-	readinessProbe, livelinessProbe *apiv1.Probe,
-	metadata map[string]interface{}) K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		containerPorts := []apiv1.ContainerPort{}
-		if appPort > 0 {
-			containerPorts = append(containerPorts, containerPortHelper("http", appPort, apiv1.ProtocolTCP))
-		}
-
-		if metricsPort > 0 {
-			containerPorts = append(containerPorts, containerPortHelper("metrics", metricsPort, apiv1.ProtocolTCP))
-			if d.Spec.Template.ObjectMeta.Annotations == nil {
-				d.Spec.Template.ObjectMeta.Annotations = map[string]string{
-					"prometheus.io/scrape": "true",
-					"prometheus.io/port":   strconv.Itoa(metricsPort),
-				}
-			} else {
-				d.Spec.Template.ObjectMeta.Annotations["prometheus.io/scrape"] = "true"
-				d.Spec.Template.ObjectMeta.Annotations["prometheus.io/port"] = strconv.Itoa(metricsPort)
-			}
-		}
-
-		var containerEnvVars []apiv1.EnvVar
-
-		for k, v := range metadata {
-			value, ok := v.(string)
-			if !ok {
-				continue
-			}
-			containerEnvVars = append(containerEnvVars, apiv1.EnvVar{
-				Name:  k,
-				Value: value,
-			})
-		}
-
-		container := apiv1.Container{
-			Name:            name,
-			Image:           image,
-			ImagePullPolicy: apiv1.PullAlways,
-			Ports:           containerPorts,
-			LivenessProbe:   livelinessProbe,
-			ReadinessProbe:  readinessProbe,
-			Env:             containerEnvVars,
-		}
-
-		if d.Spec.Template.Spec.Containers == nil {
-			d.Spec.Template.Spec.Containers = []apiv1.Container{container}
-		} else {
-			d.Spec.Template.Spec.Containers = append(d.Spec.Template.Spec.Containers, container)
-		}
-
-	}
-}
-
-func deploymentReplicasOpt(count int) K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		d.Spec.Replicas = int32Ptr(count)
-	}
-}
-
-func deploymentSecurityContextOpt(runAs int) K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		if d.Spec.Template.Spec.SecurityContext == nil {
-			d.Spec.Template.Spec.SecurityContext = &apiv1.PodSecurityContext{
-				RunAsGroup: int64Ptr(int64(runAs)),
-				RunAsUser:  int64Ptr(int64(runAs)),
-				FSGroup:    int64Ptr(int64(fsGroup)),
-			}
-		} else {
-			d.Spec.Template.Spec.SecurityContext.RunAsGroup = int64Ptr(int64(runAs))
-			d.Spec.Template.Spec.SecurityContext.RunAsUser = int64Ptr(int64(runAs))
-			d.Spec.Template.Spec.SecurityContext.FSGroup = int64Ptr(int64(fsGroup))
-
-		}
-	}
-}
-
-func deploymentServiceAccountOpt(sa string) K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		d.Spec.Template.Spec.ServiceAccountName = sa
-	}
-}
-
-func deploymentLabelsOpt(name, version, nuance string) K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		if d.Spec.Template.ObjectMeta.Labels == nil {
-			d.Spec.Template.ObjectMeta.Labels = map[string]string{
-				"app":     name,
-				"version": version,
-				"nuance":  nuance,
-			}
-		} else {
-			d.Spec.Template.ObjectMeta.Labels["app"] = name
-			d.Spec.Template.ObjectMeta.Labels["version"] = version
-			d.Spec.Template.ObjectMeta.Labels["nuance"] = nuance
-		}
-	}
-}
-
-func deploymentNilCheck(d *appsv1.Deployment) *appsv1.Deployment {
-	if d == nil {
-		return &appsv1.Deployment{}
-	} else {
-		return d
-	}
-
-}
-
-func deploymentSelectorLabelsOpt(name string) K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		if d.Spec.Selector == nil {
-			d.Spec.Selector = &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": name,
+func setupK8sService(serviceName, namespace string, servicePort int, stickySessions bool) *apiv1.Service {
+	service := &apiv1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceName,
+			Namespace: namespace,
+		},
+		Spec: apiv1.ServiceSpec{
+			Ports: []apiv1.ServicePort{
+				{
+					Port: int32(servicePort),
+					TargetPort: intstr.IntOrString{
+						Type:   intstr.Int,
+						IntVal: int32(servicePort),
+					},
 				},
-			}
-		} else {
-			d.Spec.Selector.MatchLabels["app"] = name
+			},
+			Selector: map[string]string{
+				"app": serviceName,
+			},
+		},
+	}
+
+	if stickySessions {
+		service.Spec.SessionAffinity = apiv1.ServiceAffinityClientIP
+	}
+
+	return service
+}
+
+func getK8sDeployment(
+	instanceCount, runAs int,
+	serviceAccountName,
+	serviceName,
+	artifactName,
+	artifactVersion,
+	namespace,
+	containerImage,
+	nuance string) *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      serviceName,
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(instanceCount),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": serviceName,
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app":     serviceName,
+						"version": artifactVersion,
+						"nuance":  nuance,
+					},
+				},
+				Spec: apiv1.PodSpec{
+					SecurityContext: &apiv1.PodSecurityContext{
+						RunAsUser:  int64Ptr(int64(runAs)),
+						RunAsGroup: int64Ptr(int64(runAs)),
+						FSGroup:    int64Ptr(65534),
+					},
+					ServiceAccountName: serviceAccountName,
+					Containers: []apiv1.Container{
+						{
+							Name:            artifactName,
+							ImagePullPolicy: apiv1.PullAlways,
+							Image:           containerImage,
+							Ports:           []apiv1.ContainerPort{},
+						},
+					},
+					TerminationGracePeriodSeconds: int64Ptr(300),
+					ImagePullSecrets: []apiv1.LocalObjectReference{
+						{
+							Name: "docker-cfg",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func setupDeploymentEnvironment(metadata map[string]interface{}, deployment *appsv1.Deployment) {
+	var containerEnvVars []apiv1.EnvVar
+
+	for k, v := range metadata {
+		value, ok := v.(string)
+		if !ok {
+			continue
 		}
+		containerEnvVars = append(containerEnvVars, apiv1.EnvVar{
+			Name:  k,
+			Value: value,
+		})
+	}
+
+	deployment.Spec.Template.Spec.Containers[0].Env = containerEnvVars
+}
+
+func setupPorts(servicePort, metricsPort int, deployment *appsv1.Deployment) {
+	if servicePort != 0 {
+		deployment.Spec.Template.Spec.Containers[0].Ports = append(deployment.Spec.Template.Spec.Containers[0].Ports, apiv1.ContainerPort{
+			Name:          "http",
+			ContainerPort: int32(servicePort),
+			Protocol:      apiv1.ProtocolTCP,
+		})
+	}
+
+	if metricsPort != 0 {
+		deployment.Spec.Template.Spec.Containers[0].Ports = append(deployment.Spec.Template.Spec.Containers[0].Ports, apiv1.ContainerPort{
+			Name:          "metrics",
+			ContainerPort: int32(metricsPort),
+			Protocol:      apiv1.ProtocolTCP,
+		})
 	}
 }
 
-func deploymentNamespaceOpt(ns string) K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		d.ObjectMeta.Namespace = ns
+func setupMetrics(port int, deployment *appsv1.Deployment) {
+	if port == 0 {
+		return
 	}
-}
 
-func deploymentImagePullSecretsOpt() K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		dockerCreds := apiv1.LocalObjectReference{Name: dockerCreds}
-		deploymentNilCheck(d)
-		if d.Spec.Template.Spec.ImagePullSecrets == nil {
-			d.Spec.Template.Spec.ImagePullSecrets = []apiv1.LocalObjectReference{dockerCreds}
-		} else {
-			d.Spec.Template.Spec.ImagePullSecrets = append(d.Spec.Template.Spec.ImagePullSecrets, dockerCreds)
-		}
-
+	annotations := map[string]string{
+		"prometheus.io/scrape": "true",
+		"prometheus.io/port":   strconv.Itoa(port),
 	}
+
+	deployment.Spec.Template.ObjectMeta.Annotations = annotations
 }
 
-func deploymentTerminationGracePeriodOpt() K8sDeployOption {
-	return func(d *appsv1.Deployment) {
-		deploymentNilCheck(d)
-		d.Spec.Template.Spec.TerminationGracePeriodSeconds = int64Ptr(int64(terminationGracePeriodSecs))
-	}
-}
-
-func containerPortHelper(name string, port int, protocol apiv1.Protocol) apiv1.ContainerPort {
-	return apiv1.ContainerPort{Name: name, ContainerPort: int32(port), Protocol: protocol}
-}
-
-func (s *Scheduler) probeHelper(ctx context.Context, probeBytes []byte) *apiv1.Probe {
+func (s *Scheduler) setupReadinessProbe(ctx context.Context, probeBytes []byte, deployment *appsv1.Deployment) {
 	if len(probeBytes) < 5 {
-		return nil
+		return
 	}
 	var probe apiv1.Probe
 	err := json.Unmarshal(probeBytes, &probe)
 	if err != nil {
-		s.Logger(ctx).Error("failed to unmarshal the probe", zap.Error(err))
-		return nil
+		s.Logger(ctx).Warn("failed to unmarshal the readiness probe", zap.Error(err))
+		return
 	}
 	if probe.Handler.Exec == nil && probe.Handler.HTTPGet == nil && probe.Handler.TCPSocket == nil {
-		s.Logger(ctx).Error("invalid probe, the handler was not set")
-		return nil
+		s.Logger(ctx).Warn("invalid readiness probe, the handler was not set")
+		return
 	}
-	return &probe
+	deployment.Spec.Template.Spec.Containers[0].ReadinessProbe = &probe
 }
 
-// newK8sDeployment initializes an empty k8s Deployment and then applies the given K8sDeployOption options
-func newK8sDeployment(opts ...K8sDeployOption) *appsv1.Deployment {
-	d := &appsv1.Deployment{}
-	for _, opt := range opts {
-		opt(d)
+func (s *Scheduler) setupLivelinessProbe(ctx context.Context, probeBytes []byte, deployment *appsv1.Deployment) {
+	if len(probeBytes) < 5 {
+		return
 	}
-	return d
+	var probe apiv1.Probe
+	err := json.Unmarshal(probeBytes, &probe)
+	if err != nil {
+		s.Logger(ctx).Warn("failed to unmarshal the liveliness probe", zap.Error(err))
+		return
+	}
+	if probe.Handler.Exec == nil && probe.Handler.HTTPGet == nil && probe.Handler.TCPSocket == nil {
+		s.Logger(ctx).Warn("invalid liveliness probe, the handler was not set")
+		return
+	}
+	deployment.Spec.Template.Spec.Containers[0].LivenessProbe = &probe
 }
 
-// upsertDeployment will either create the initial K8s Deployment or Update an existing deployment
-func (s *Scheduler) upsertDeployment(ctx context.Context, k8s *kubernetes.Clientset, service *eve.DeployService, nuance string, plan *eve.NSDeploymentPlan, failNLog func(err error, format string, a ...interface{})) {
+func (s *Scheduler) deployDockerService(ctx context.Context, service *eve.DeployService, plan *eve.NSDeploymentPlan) {
+	failNLog := s.failAndLogFn(ctx, service.ServiceName, service.DeployArtifact, plan)
+	k8s, err := getK8sClient()
+	if err != nil {
+		failNLog(err, "an error occurred trying to get the k8s client")
+		return
+	}
+	var instanceCount = service.Count
+	timeNuance := strconv.Itoa(int(time.Now().Unix()))
+	imageName := getDockerImageName(service.DeployArtifact)
+	deployment := getK8sDeployment(
+		instanceCount, service.RunAs,
+		service.ServiceAccount,
+		service.ServiceName,
+		service.ArtifactName,
+		service.AvailableVersion,
+		plan.Namespace.Name,
+		imageName,
+		timeNuance)
+	setupDeploymentEnvironment(service.Metadata, deployment)
+	setupMetrics(service.MetricsPort, deployment)
+	setupPorts(service.ServicePort, service.MetricsPort, deployment)
+	s.setupLivelinessProbe(ctx, service.LivelinessProbe, deployment)
+	s.setupReadinessProbe(ctx, service.ReadinessProbe, deployment)
 
-	deployment := newK8sDeployment(
-		deploymentContainerOpt(
-			service.ServicePort,
-			service.MetricsPort,
-			service.ArtifactName,
-			getDockerImageName(service.DeployArtifact),
-			s.probeHelper(ctx, service.ReadinessProbe),
-			s.probeHelper(ctx, service.LivelinessProbe),
-			service.Metadata,
-		),
-		deploymentSelectorLabelsOpt(service.ServiceName),
-		deploymentLabelsOpt(service.ServiceName, service.AvailableVersion, nuance),
-		deploymentSecurityContextOpt(service.RunAs),
-		deploymentReplicasOpt(service.Count),
-		deploymentServiceAccountOpt(service.ServiceAccount),
-		deploymentNamespaceOpt(plan.Namespace.Name),
-		deploymentImagePullSecretsOpt(),
-		deploymentTerminationGracePeriodOpt(),
-	)
+	if service.ServicePort > 0 {
+		_, err := k8s.CoreV1().Services(plan.Namespace.Name).Get(ctx, service.ServiceName, metav1.GetOptions{})
+		if err != nil {
+			if k8sErrors.IsNotFound(err) {
+				_, err := k8s.CoreV1().Services(plan.Namespace.Name).Create(ctx,
+					setupK8sService(service.ServiceName, plan.Namespace.Name, service.ServicePort, service.StickySessions), metav1.CreateOptions{})
+				if err != nil {
+					failNLog(err, "an error occurred trying to create the service")
+					return
+				}
+			} else {
+				failNLog(err, "an error occurred trying to check for the service")
+				return
+			}
+		}
+	}
 
-	_, err := k8s.AppsV1().Deployments(plan.Namespace.Name).Get(ctx, service.ServiceName, metav1.GetOptions{})
+	_, err = k8s.AppsV1().Deployments(plan.Namespace.Name).Get(ctx, service.ServiceName, metav1.GetOptions{})
 	if err != nil {
 		if k8sErrors.IsNotFound(err) {
 			// This app hasn't been deployed yet so we need to deploy it
@@ -335,60 +261,12 @@ func (s *Scheduler) upsertDeployment(ctx context.Context, k8s *kubernetes.Client
 			return
 		}
 	}
-}
 
-// ensureK8sServiceExists Ensures that the K8s Service exists, if not, then we create it
-func (s *Scheduler) ensureServiceExists(
-	ctx context.Context,
-	k8s *kubernetes.Clientset,
-	service *eve.DeployService,
-	plan *eve.NSDeploymentPlan,
-	failNLog func(err error, format string, a ...interface{}),
-) {
-
-	if service.ServicePort > 0 {
-		_, err := k8s.CoreV1().Services(plan.Namespace.Name).Get(ctx, service.ServiceName, metav1.GetOptions{})
-		if err != nil {
-			if k8sErrors.IsNotFound(err) {
-				_, err := k8s.CoreV1().Services(plan.Namespace.Name).Create(
-					ctx,
-					newK8sService(
-						svcNameOpt(service.ServiceName),
-						svcNamespaceOpt(plan.Namespace.Name),
-						svcPortOpt(service.ServicePort),
-						svcStickySessionsOpt(service.StickySessions),
-					),
-					metav1.CreateOptions{},
-				)
-				if err != nil {
-					failNLog(err, "an error occurred trying to create the service")
-					return
-				}
-			} else {
-				failNLog(err, "an error occurred trying to check for the service")
-				return
-			}
-		}
-	}
-}
-
-func labelSelectorHelper(svc *eve.DeployService, nuance string) string {
-	return fmt.Sprintf("app=%s,version=%s,nuance=%s", svc.ServiceName, svc.AvailableVersion, nuance)
-}
-
-func (s *Scheduler) watchPodStatus(
-	ctx context.Context,
-	k8s *kubernetes.Clientset,
-	service *eve.DeployService,
-	nuance string,
-	plan *eve.NSDeploymentPlan,
-	failNLog func(err error, format string, a ...interface{}),
-) {
-
+	labelSelector := fmt.Sprintf("app=%s,version=%s,nuance=%s", service.ServiceName, service.AvailableVersion, timeNuance)
 	pods := k8s.CoreV1().Pods(plan.Namespace.Name)
-	podWatcher, err := pods.Watch(ctx, metav1.ListOptions{
+	watch, err := pods.Watch(ctx, metav1.ListOptions{
 		TypeMeta:       metav1.TypeMeta{},
-		LabelSelector:  labelSelectorHelper(service, nuance),
+		LabelSelector:  labelSelector,
 		TimeoutSeconds: int64Ptr(config.GetConfig().K8sDeployTimeoutSec),
 	})
 	if err != nil {
@@ -397,19 +275,18 @@ func (s *Scheduler) watchPodStatus(
 	}
 	started := make(map[string]bool)
 
-	var instanceCount = service.Count
 	if strings.HasPrefix(service.ServiceName, "eve-sch") {
 		instanceCount = 1
 	}
 
-	for event := range podWatcher.ResultChan() {
+	for event := range watch.ResultChan() {
 		p, ok := event.Object.(*apiv1.Pod)
 		if !ok {
 			continue
 		}
 		for _, x := range p.Status.ContainerStatuses {
 			if x.LastTerminationState.Terminated != nil {
-				failNLog(nil, "pod failed to start and returned a non zero exit code: %v", x.LastTerminationState.Terminated.ExitCode)
+				failNLog(nil, "pod failed to start and returned a non zero exit code: %d", x.LastTerminationState.Terminated.ExitCode)
 				continue
 			}
 			if !*x.Started {
@@ -419,13 +296,15 @@ func (s *Scheduler) watchPodStatus(
 		}
 
 		if len(started) == instanceCount {
-			podWatcher.Stop()
+			watch.Stop()
 		}
 	}
 
 	if len(started) != instanceCount {
 		// make sure we don't get a false positive and actually check
-		pods, err := k8s.CoreV1().Pods(plan.Namespace.Name).List(ctx, metav1.ListOptions{LabelSelector: labelSelectorHelper(service, nuance)})
+		pods, err := k8s.CoreV1().Pods(plan.Namespace.Name).List(ctx, metav1.ListOptions{
+			LabelSelector: labelSelector,
+		})
 		if err != nil {
 			failNLog(nil, "an error occurred while trying to deploy: %s, timed out waiting for app to start.", service.ServiceName)
 			return
@@ -448,46 +327,6 @@ func (s *Scheduler) watchPodStatus(
 			return
 		}
 	}
-}
 
-func (s *Scheduler) deployDockerService(ctx context.Context, service *eve.DeployService, plan *eve.NSDeploymentPlan) {
-	s.Logger(ctx).Debug("deploying docker service", zap.String("artifact_name", service.ArtifactName))
-
-	// establish the failure handler func
-	failNLog := s.failAndLogFn(ctx, service.ServiceName, service.DeployArtifact, plan)
-
-	// get the k8s API Client
-	k8s, err := getK8sClient()
-	if err != nil {
-		failNLog(err, "an error occurred trying to get the k8s client")
-		return
-	}
-
-	if k8s == nil {
-		failNLog(nil, "nil k8s client")
-		return
-	}
-
-	if service == nil {
-		failNLog(nil, "nil eve deploy service")
-		return
-	}
-
-	if plan == nil {
-		failNLog(nil, "nil eve deploy plan")
-		return
-	}
-
-	// ensure the k8s service exists
-	s.ensureServiceExists(ctx, k8s, service, plan, failNLog)
-
-	nuance := strconv.Itoa(int(time.Now().Unix()))
-	// create or update the deployment
-	s.upsertDeployment(ctx, k8s, service, nuance, plan, failNLog)
-
-	// watch pod status changes
-	s.watchPodStatus(ctx, k8s, service, nuance, plan, failNLog)
-
-	// Return the successful result
 	service.Result = eve.DeployArtifactResultSuccess
 }
