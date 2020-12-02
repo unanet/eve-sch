3c34a897fe94a8bbfba0a30bf7d37bb0412ebe1a
Troy Sampson <troy.j.sampson@gmail.com>
2020-12-01 18:43:25 -0700

    remove label matchers

--- internal/service/k8s-deploy_test.go
+++ internal/service/k8s-deploy_test.go
@@ -1,13 +1,16 @@
-// +build local
-
 package service
 
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"gitlab.unanet.io/devops/eve/pkg/log"
+	"go.uber.org/zap"
+	apiv1 "k8s.io/api/core/v1"
 
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -46,8 +49,47 @@ func TestScheduler_deployNamespace(t *testing.T) {
 	k8s := GetK8sClient(t)
 	ctx := context.TODO()
 
-	job, err := k8s.BatchV1().Jobs("una-int-current").Get(ctx, "unaneta-migration", metav1.GetOptions{})
+	job, err := k8s.BatchV1().Jobs("una-dev-current").Get(ctx, "hello-world", metav1.GetOptions{})
 	require.NoError(t, err)
 	require.NotNil(t, job)
+}
 
+func TestThis(t *testing.T) {
+	log.Logger.Info("TROY")
+	k8s := GetK8sClient(t)
+	ctx := context.TODO()
+	pods := k8s.CoreV1().Pods("una-dev-current")
+	watch, err := pods.Watch(ctx, metav1.ListOptions{
+		TypeMeta:       metav1.TypeMeta{},
+		LabelSelector:  fmt.Sprintf("job=%s", "hello-world"),
+		TimeoutSeconds: int64Ptr(10),
+	})
+	require.NoError(t, err)
+	require.NotNil(t, watch)
+
+	started := make(map[string]bool)
+
+	log.Logger.Info("TROY Event", zap.Any("container status", ""))
+	for event := range watch.ResultChan() {
+		p, ok := event.Object.(*apiv1.Pod)
+		if !ok {
+			continue
+		}
+		log.Logger.Info("TROY POD Event", zap.Any("container status", p))
+		for _, x := range p.Status.ContainerStatuses {
+			log.Logger.Info("TROY Container Status", zap.Any("container status", x))
+			if x.LastTerminationState.Terminated != nil {
+				watch.Stop()
+			}
+
+			if !x.Ready {
+				continue
+			}
+			started[p.Name] = true
+		}
+
+		if len(started) >= 1 {
+			watch.Stop()
+		}
+	}
 }
--- internal/service/k8s-job.go
+++ internal/service/k8s-job.go
@@ -156,11 +156,12 @@ func (s *Scheduler) setupK8sJob(ctx context.Context, k8s *kubernetes.Clientset, 
 	log.Logger.Info("TROY", zap.Any("Template.ObjectMeta.Labels", existingJob.Spec.Template.ObjectMeta.Labels))
 
 	newJob.Spec.Selector = existingJob.Spec.Selector
-	newJob.ObjectMeta.Labels = existingJob.ObjectMeta.Labels
-	newJob.Spec.Template.ObjectMeta.Labels = existingJob.Spec.Template.ObjectMeta.Labels
+	//newJob.ObjectMeta.Labels = existingJob.ObjectMeta.Labels
+	//newJob.Spec.Template.ObjectMeta.Labels = existingJob.Spec.Template.ObjectMeta.Labels
 	if _, err = k8s.BatchV1().Jobs(plan.Namespace.Name).Update(ctx, newJob, metav1.UpdateOptions{TypeMeta: jobMetaData}); err != nil {
 		return errors.Wrap(err, "an error occurred trying to update the existing job")
 	}
+	log.Logger.Info("TROY", zap.Any("success update", true))
 	return nil
 }
 
