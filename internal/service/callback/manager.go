aab80e2d7fa5af057f15faa3c0685a7f270f8e02
Troy Sampson <tsampson@sixgill.com>
2021-09-05 14:22:39 -0600

    update eve schedule to log the message rather than send them back to eve api

--- internal/service/callback/manager.go
+++ internal/service/callback/manager.go
@@ -2,14 +2,12 @@ package callback
 
 import (
 	"context"
-	"encoding/json"
 
 	uuid "github.com/satori/go.uuid"
 	"github.com/unanet/eve/pkg/eve"
 	"github.com/unanet/eve/pkg/queue"
-	"github.com/unanet/go/pkg/errors"
-
-	"github.com/unanet/eve-sch/internal/service"
+	"github.com/unanet/go/pkg/log"
+	"go.uber.org/zap"
 )
 
 func NewManager(w *queue.Worker, apiQUrl string) *Manager {
@@ -25,22 +23,24 @@ type Manager struct {
 }
 
 func (m *Manager) Callback(ctx context.Context, id uuid.UUID, message eve.CallbackMessage) error {
-	messageJson, err := json.Marshal(message)
-	if err != nil {
-		return errors.Wrap(err)
-	}
+	// messageJson, err := json.Marshal(message)
+	// if err != nil {
+	// 	return errors.Wrap(err)
+	// }
 
-	err = m.worker.Message(ctx, m.apiQUrl, &queue.M{
-		ID:       id,
-		GroupID:  service.CommandCallbackMessage,
-		Body:     messageJson,
-		Command:  service.CommandCallbackMessage,
-		DedupeID: uuid.NewV4().String(),
-	})
+	log.Logger.Info("Callback Message(s) Received", zap.String("message_id", id.String()), zap.Strings("messages", message.Messages))
 
-	if err != nil {
-		return errors.Wrap(err)
-	}
+	// err = m.worker.Message(ctx, m.apiQUrl, &queue.M{
+	// 	ID:       id,
+	// 	GroupID:  service.CommandCallbackMessage,
+	// 	Body:     messageJson,
+	// 	Command:  service.CommandCallbackMessage,
+	// 	DedupeID: uuid.NewV4().String(),
+	// })
+
+	// if err != nil {
+	// 	return errors.Wrap(err)
+	// }
 
 	return nil
 }
